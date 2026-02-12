81fd360f4007560c0f93275d3537e1f877f5c2cb
Jwfigueiredo <jwfigueiredo10@gmail.com>
2026-02-12 02:39:32 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 projeto para aprender programação-curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-function Home(){
-    return <h1>Só quem se arrisca merece viver o extraordinário!!!</h1>
+function Home() {
+  return <h1>Só quem se arrisca merece viver o extraordinário!!!</h1>;
 }
 export default Home;
+
+function teste2() {
+  console.log("indentação errada");
+}
